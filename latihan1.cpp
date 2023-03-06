b9a622533524d29e330bef33cbf7f39dc867f829
dewikhulbi <dewikhlb20@gmail.com>
2023-03-06 11:46:52 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Begin
+    // Numeric Luas, p, l
+    double Luas, p, l;
+    // diplay 'Masukan Panjangnya = '
+    cout << "Masukan Panjangnya = ";
+    // Accept p
+    cin >> p;
+    // display 'Masukan Lebarnya = '
+    cout << "Masukan Lebarnya = ";
+    // Accept l
+    cin >> l;
+    // compute luas = p*l;
+    Luas = p*l;
+    // display "Luasnya = "+Luas
+    cout << "Luasnya = " << Luas;
+    //end 
+}
---
+++ pd.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello!" << endl;
+    cout << "ini adalah kelas C" << endl;
+
+    return 0;
+}
